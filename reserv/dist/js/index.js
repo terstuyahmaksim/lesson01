0a3ae62599468cd911ed3b9b870fe1d54e24b596
Terstuyah Maksim <terstuyah.maksim@gmail.com>
2019-09-25 20:48:35 +0300

    markup ready

---
+++ reserv/dist/js/index.js
@@ -0,0 +1,8 @@
+// var block = document.getElementsByClassName('menu__item');
+// var activeClass = document.getElementsByClassName('--active');
+// block[0].onclick = function() {
+//   activeClass[0].classList.add("--active");
+// }
+// block[1].onclick = function() {
+//   activeClass[0].classList.remove("--active");
+// }
